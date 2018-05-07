9a862c350b9576164b7cee7a30a4981db13ff9e7
tallal-javed <tallal.javed@outlook.com>
2018-05-07 11:28:25 +0500

    Initial Commit

---
+++ GivBit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GivBit
+//
+//  Created by Tallal Javed on 5/7/18.
+//  Copyright © 2018 Ibtidah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
