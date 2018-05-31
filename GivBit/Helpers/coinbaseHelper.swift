b11f359b927244c09e3c749387d4083bf5dac54d
tabish0027 <tallal.javed@outlook.com>
2018-06-01 00:14:37 +0500

    coinable added

--- GivBit/Helpers/coinbaseHelper.swift
+++ GivBit/Helpers/coinbaseHelper.swift
@@ -13,11 +13,11 @@ import OAuthSwift
 class coinbaseoauth : NSObject{
     
     static var sharedInstnace = coinbaseoauth()
-    let meta_all_data = ["":"meta[send_limit_period]=day&meta[send_limit_currency]=USD&meta[send_limit_amount]=1.00&account=all"]
+    let meta_all_data = ["":"meta[send_limit_period]=day&meta[send_limit_currency]=USD&meta[send_limit_amount]=500.00&account=all"]
     
     var loginvc = LoginVC()
     var oauthswift = OAuth2Swift(
-        consumerKey:    "2303625dda9f8ed8a5dd76f111106897069b8b2a6126972718240d50315111a7",
+        consumerKey:    "723e663bdd30aac0f9641160de28ce520e1a065853febbd9a9c983569753bcf3",
         consumerSecret: "",
         authorizeUrl: "https://www.coinbase.com/oauth/authorize/",
         accessTokenUrl: "http://www.coinbase.com/oauth/token"  ,
